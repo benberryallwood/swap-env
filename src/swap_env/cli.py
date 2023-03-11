0f1fb6d89928092a57c0895ab88284bca34348a8
Ben Berry-Allwood <benberryallwood@gmail.com>
2023-03-11 21:59:31 +0000

    Skip prompt and exit program with q or escape

--- src/swap_env/cli.py
+++ src/swap_env/cli.py
@@ -4,11 +4,25 @@ from InquirerPy import inquirer
 from swap_env.dotenv_files import DotenvFiles
 
 
+keybindings = {
+    "answer": [{"key": "enter"}],  # answer the prompt
+    "interrupt": [{"key": "c-c"}],  # raise KeyboardInterrupt
+    "skip": [{"key": "q"}, {"key": "escape"}],  # skip the prompt
+}
+
+
 def app():
     dotenv_files = DotenvFiles(Path.home().joinpath(".swap_env"))
 
     dotenv_file_choice = inquirer.select(
-        message="Select a .env file", choices=list(dotenv_files), vi_mode=True
+        message="Select a .env file",
+        choices=list(dotenv_files),
+        vi_mode=True,
+        mandatory=False,
+        keybindings=keybindings,
     ).execute()
+
+    if dotenv_file_choice is None:
+        return
 
     dotenv_files.link(dotenv_file_choice)
