abce931b4483365e9cf083401b8300728116a0f6
Ben Berry-Allwood <benberryallwood@gmail.com>
2023-03-11 18:32:22 +0000

    Reword .env to dotenv in prompt

--- src/swap_env/cli.py
+++ src/swap_env/cli.py
@@ -7,7 +7,7 @@ def app():
     dotenv_files = DotenvFiles()
 
     dotenv_file_choice = inquirer.select(
-        message="Select a dotenv file", choices=list(dotenv_files), vi_mode=True
+        message="Select a .env file", choices=list(dotenv_files), vi_mode=True
     ).execute()
 
     dotenv_files.link(dotenv_file_choice)
