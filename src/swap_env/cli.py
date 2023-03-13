b73706c7a9623191a50325198a47c1c57da37d81
Ben Berry-Allwood <benberryallwood@gmail.com>
2023-03-13 19:41:02 +0000

    Add logic to save existing .env before linking

--- src/swap_env/cli.py
+++ src/swap_env/cli.py
@@ -14,15 +14,39 @@ keybindings = {
 }
 
 
+def prompt_to_save_current_file_if_exists(dotenv_files: DotenvFiles) -> None:
+    local_dotenv_path = Path.cwd().joinpath(".env")
+    if not local_dotenv_path.exists() or local_dotenv_path.is_symlink():
+        return
+
+    save_local_file = inquirer.confirm(
+        message="A .env file already exists, do you want to save this before continuing?",
+        default=True,
+    ).execute()
+
+    if not save_local_file:
+        return
+
+    new_dotenv_name = inquirer.text(
+        message="Enter a name for this .env file:",
+        validate=lambda name: name not in dotenv_files and name != "",
+        invalid_message="The name is empty or already exists",
+    ).execute()
+
+    dotenv_files.save_local_dotenv(name=new_dotenv_name)
+
+
 def app():
     dotenv_files = DotenvFiles(path=DOTENV_DIR)
+
+    prompt_to_save_current_file_if_exists(dotenv_files=dotenv_files)
 
     if not dotenv_files:
         print("There are no .env files in ~/.swap-env/")
         return
 
     dotenv_file_choice: Optional[str] = inquirer.select(
-        message="Select a .env file",
+        message="Select a .env file:",
         choices=list(dotenv_files),
         vi_mode=True,
         mandatory=False,
