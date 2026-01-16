d6f954e80c4643b7392a6914218a9a4471715c7d
Ben Berry-Allwood <b.berry-allwood@protonmail.com>
2026-01-16 21:46:08 +0000

    Use prompt_toolkit instead of InquirerPy

--- src/swap_env/cli.py
+++ src/swap_env/cli.py
@@ -1,17 +1,10 @@
-from __future__ import annotations
-
 from pathlib import Path
 
-from InquirerPy import inquirer
+from prompt_toolkit.shortcuts import choice, confirm, prompt
+from prompt_toolkit.validation import Validator
 
 from swap_env.dotenv_dir import search_fs_for_dotenv_dir
 from swap_env.dotenv_files import DotenvFiles
-
-keybindings = {
-    "answer": [{"key": "enter"}],  # answer the prompt
-    "interrupt": [{"key": "c-c"}],  # raise KeyboardInterrupt
-    "skip": [{"key": "q"}, {"key": "escape"}],  # skip the prompt
-}
 
 
 def prompt_to_save_current_file_if_exists(dotenv_files: DotenvFiles) -> None:
@@ -19,19 +12,20 @@ def prompt_to_save_current_file_if_exists(dotenv_files: DotenvFiles) -> None:
     if not local_dotenv_path.exists() or local_dotenv_path.is_symlink():
         return
 
-    save_local_file = inquirer.confirm(
-        message="A .env file already exists, do you want to save this before continuing?",
-        default=True,
-    ).execute()
+    save_local_file = confirm(
+        "A .env file already exists, do you want to save this before continuing?"
+    )
 
     if not save_local_file:
         return
 
-    new_dotenv_name = inquirer.text(
-        message="Enter a name for this .env file:",
-        validate=lambda name: name not in dotenv_files and name != "",
-        invalid_message="The name is empty or already exists",
-    ).execute()
+    new_dotenv_name = prompt(
+        "Enter a name for this .env file: ",
+        validator=Validator.from_callable(
+            lambda name: name not in dotenv_files and name != "",
+            error_message="The name is empty or already exists",
+        ),
+    )
 
     dotenv_files.save_local_dotenv(name=new_dotenv_name)
 
@@ -46,15 +40,9 @@ def app():
         print(f"No dotenv files found in {dotenv_files.path}")
         return
 
-    dotenv_file_choice: str | None = inquirer.select(
-        message="Select a .env file:",
-        choices=list(dotenv_files),
-        vi_mode=True,
-        mandatory=False,
-        keybindings=keybindings,
-    ).execute()
-
-    if dotenv_file_choice is None:  # prompt skipped with 'q' or 'escape'
-        return
+    dotenv_file_choice = choice(
+        "Select a .env file:",
+        options=[(name, name) for name in dotenv_files],
+    )
 
     dotenv_files.link(dotenv_file_choice)
